488fa536e685decdeaf8b64885d7147c5432fee9
NothingWeAre <arsgran@gmail.com>
2017-08-02 15:18:18 +0300

    WIP: Tests

--- Tests/HelicopterVehicleTest.php
+++ Tests/HelicopterVehicleTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Exceptions\IncorrectFuelTypeException;
+use Exceptions\NoFuelException;
+use Exceptions\NotInFlightException;
 use PHPUnit\Framework\TestCase;
 use Vehicles\AbstractVehicle;
 use Vehicles\HelicopterVehicle;
@@ -11,8 +13,8 @@ class HelicopterVehicleTest extends TestCase
     public function testInstantiation()
     {
         $vehicle = new HelicopterVehicle('test');
-        $this->assertInstanceOf(AbstractVehicle::class, $vehicle,'Must extend AbstractVehicle');
-        $this->assertInstanceOf(AerialInterface::class, $vehicle,'Must implement AerialInterface');
+        $this->assertInstanceOf(AbstractVehicle::class, $vehicle, 'Must extend AbstractVehicle');
+        $this->assertInstanceOf(AerialInterface::class, $vehicle, 'Must implement AerialInterface');
         return $vehicle;
     }
 
@@ -24,31 +26,73 @@ class HelicopterVehicleTest extends TestCase
     public function testRefuelWithIncorrectFuel(HelicopterVehicle $vehicle)
     {
         $this->expectException(IncorrectFuelTypeException::class);
-
         $vehicle->refuel('not fuel');
     }
-
 
     /**
      * @depends testInstantiation
      *
      * @param HelicopterVehicle $vehicle
      */
-    public function testRefuelWithAcceptedFuelType(HelicopterVehicle $vehicle){
+    public function testTakeOffWithEmptyTank(HelicopterVehicle $vehicle)
+    {
+        $this->expectException(NoFuelException::class);
+        $vehicle->takeOff();
+    }
+
+    /**
+     * @depends testInstantiation
+     *
+     * @param HelicopterVehicle $vehicle
+     */
+    public function testFlyWithEmptyTank(HelicopterVehicle $vehicle)
+    {
+        $this->expectException(NoFuelException::class);
+        $vehicle->fly();
+    }
+
+    /**
+     * @depends testInstantiation
+     *
+     * @param HelicopterVehicle $vehicle
+     *
+     * @return HelicopterVehicle
+     */
+    public function testRefuelWithAcceptedFuelType(HelicopterVehicle $vehicle)
+    {
         $this->assertEquals(
             'refueled',
             $vehicle->refuel($vehicle->getAcceptedFuelType())
         );
+
+        return $vehicle;
     }
 
-
-    public function testOperations(HelicopterVehicle $vehicle)
+    /**
+     * @depends testRefuelWithAcceptedFuelType
+     *
+     * @param HelicopterVehicle $vehicle
+     */
+    public function testFlyWithoutTakeOff(HelicopterVehicle $vehicle)
     {
+        $this->expectException(NotInFlightException::class);
+        $vehicle->fly();
+    }
 
+    /**
+     * @depends testRefuelWithAcceptedFuelType
+     *
+     * @param HelicopterVehicle $vehicle
+     *
+     * @return HelicopterVehicle
+     */
+    public function testTakeOff(HelicopterVehicle $vehicle)
+    {
         $this->assertEquals(
             'Take-off action successful',
             $vehicle->takeOff()
         );
 
+        return $vehicle;
     }
 }
